954ab93861d708562319f99ff264ca9a02f9dbb9
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-10-09 10:28:37 -0600

    read by word

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn parse_words(file_path: &str) {
+    let file = File::open(file_path).expect("failed to open file");
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.expect("failed to read");
+        for word in line.split_whitespace() {
+            println!("{}", word);
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,6 +20,7 @@ fn main() {
         eprintln!("Usage <file_path>");
         return;
     }
-    let contents = fs::read_to_string(args[1].clone()).expect("Failed to read file");
-    println!("{}", contents);
+    parse_words(&args[1]);
+    //let contents = fs::read_to_string(args[1].clone()).expect("Failed to read file");
+    //println!("{}", contents);
 }

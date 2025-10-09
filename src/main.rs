908cb1c5f0e7b8a7ec580669927ad5c7a1c9261b
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-10-09 10:03:45 -0600

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage <file_path>");
+        return;
+    }
+    let contents = fs::read_to_string(args[1].clone()).expect("Failed to read file");
+    println!("{}", contents);
 }

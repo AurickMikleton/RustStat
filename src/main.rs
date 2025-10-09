7afceec1237999200d40b437ef68b0f49b2cb3a2
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-10-09 10:53:15 -0600

    most common word

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::BTreeMap;
 
-fn parse_words(file_path: &str) {
+fn parse_words(file_path: &str)  -> Result<(String, usize), Box<dyn std::error::Error>> {
     let file = File::open(file_path).expect("failed to open file");
     let reader = BufReader::new(file);
+    let mut tree = BTreeMap::new();
 
     for line in reader.lines() {
         let line = line.expect("failed to read");
-        for word in line.split_whitespace() {
-            println!("{}", word);
+        for word in line.split_whitespace().map(|w| w.to_lowercase()) {
+            *tree.entry(word).or_insert(0) += 1;
+            //println!("{}", word);
         }
+    }
+
+    let max_entry = tree.into_iter().max_by_key(|(_, count)| *count);
+    match max_entry {
+        Some((word, count)) => Ok((word, count)),
+        None => Err("No words".into()),
     }
 }
 
@@ -20,7 +29,6 @@ fn main() {
         eprintln!("Usage <file_path>");
         return;
     }
-    parse_words(&args[1]);
-    //let contents = fs::read_to_string(args[1].clone()).expect("Failed to read file");
-    //println!("{}", contents);
+    let word = parse_words(&args[1]).expect("");
+    println!("{} / {}", word.0, word.1);
 }

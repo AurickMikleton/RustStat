16a9b72ece3b3429c0c5be8ecaaad37b59b0fd21
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2025-10-14 17:27:15 -0600

    dang the hashsets do exist

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,17 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
+use std::collections::HashSet;
 
-fn preprocess_banlist(file_path: &str) -> HashMap<String, usize> {
+fn preprocess_banlist(file_path: &str) -> HashSet<String> {
     let file = File::open(file_path).expect("failed to open file");
     let reader = BufReader::new(file);
-    let mut words: HashMap<String, usize> = HashMap::new();
+    let mut words: HashSet<String> = HashSet::new();
     for line in reader.lines() {
         let line = line.expect("failed to read");
         for word in line.split_whitespace().map(|w| w.to_lowercase()) { // I don't want to add
                                                                         // split by commas
-            *words.entry(word).or_insert(0) += 1;
+            words.insert(word);
         }
     }
     return words;
@@ -32,11 +33,11 @@ fn parse_words(file_path: &str)  -> HashMap<String, usize> {
     return words;
 }
 
-fn check_banned(banned: &HashMap<String, usize>, word: &str) -> bool {
-    banned.contains_key(word)
+fn check_banned(banned: &HashSet<String>, word: &str) -> bool {
+    banned.contains(word)
 }
 
-fn sort_words(banned: &HashMap<String, usize>, words: HashMap<String, usize>) {
+fn sort_words(banned: &HashSet<String>, words: &HashMap<String, usize>) {
     let mut hash_vector: Vec<(&String, &usize)> = words.iter().collect();
     hash_vector.sort_by(|a, b| b.1.cmp(a.1));
 
@@ -57,5 +58,5 @@ fn main() {
     }
     let words = parse_words(&args[1]);
     let banned = preprocess_banlist(&args[2]);
-    sort_words(&banned, words);
+    sort_words(&banned, &words);
 }
